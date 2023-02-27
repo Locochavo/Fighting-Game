0dc47c2915fd469ef92e4f2ce858c46548f18d84
Allan Torndal <acesgamedevelopment@gmail.com>
2023-02-28 00:24:30 +0100

    Add project files.

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,149 @@
+using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
+using UnityEditor.Tilemaps;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class EnemyController : MonoBehaviour
+{
+    [SerializeField] private float worldLimtmin;
+    [SerializeField] private float worldLimtmax;
+
+    private Raycast ray;
+    private Stats stats;
+    private CharacterAnimations characterAnimations;
+    public bool isJumping;
+    public bool isFacingRight;
+    public bool isGrounded;
+    private Rigidbody2D rb;
+    private GameObject PlayerTarget;
+    [SerializeField] private float jumpspeed;
+    [SerializeField] private float movementSpeed;
+    [SerializeField] private float attackDistance = 2f;
+    [SerializeField] private float groundCheckRadius = 0.2f;
+    private float JumpTimer = 0f;
+    [SerializeField] private float jumpcooldown;
+    [SerializeField] Transform groundCheckCollider;
+    [SerializeField] LayerMask groundLayer;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        characterAnimations = GetComponent<CharacterAnimations>();
+        ray = GetComponent<Raycast>();
+        stats = GetComponent<Stats>();
+        isFacingRight = true;
+    }
+
+    private void Update()
+    {
+        AILogic();
+        GroundCheck();
+
+    }
+
+    private void AILogic()
+    {
+        if (worldLimtmin < transform.position.x && worldLimtmax > transform.position.x) {
+            if (isFacingRight) {
+                if (ray.LookRight().tag == "Player") {
+                    PlayerTarget = ray.LookRight();
+
+                    float distance = Vector3.Distance(transform.position, ray.LookRight().transform.position);
+
+                    Debug.Log("Right");
+                    FacingRight();
+                    if (distance < attackDistance) {
+                        Attack(PlayerTarget);
+                    }
+                    else {
+
+                        transform.position = Vector2.MoveTowards(transform.position, ray.LookRight().transform.position, movementSpeed * Time.deltaTime);
+                        characterAnimations.states = CharacterAnimations.States.Walk;
+
+                    }
+                }
+                else {
+
+                    FacingLeft();
+                    characterAnimations.states = CharacterAnimations.States.Idle;
+                }
+            }
+            else {
+                if (ray.LookLeft().tag == "Player") {
+                    PlayerTarget = ray.LookLeft();
+
+                    float distance = Vector3.Distance(transform.position, ray.LookLeft().transform.position);
+
+                    Debug.Log("Left");
+                    FacingLeft();
+                    if (distance < attackDistance) {
+
+                        Attack(PlayerTarget);
+                    }
+                    else {
+
+                        transform.position = Vector2.MoveTowards(transform.position, ray.LookLeft().transform.position, movementSpeed * Time.deltaTime);
+                        characterAnimations.states = CharacterAnimations.States.Walk;
+                    }
+                }
+                else {
+
+                    FacingRight();
+
+                }
+
+                JumpTimer += Time.deltaTime;
+                if (JumpTimer > jumpcooldown) {
+                    Jump();
+                    JumpTimer = 0;
+                }
+            }
+        }
+    }
+
+
+    private void FacingRight()
+    {
+        isFacingRight = true;
+        transform.Rotate(new Vector3(0, 0, 0));
+    }
+
+    private void FacingLeft()
+    {
+        transform.Rotate(new Vector3(0, 0, 0));
+        isFacingRight = false;
+    }
+
+    private void Attack(GameObject target)
+    {
+        characterAnimations.Punch();
+        target.GetComponent<Stats>().TakeDamage(stats.damage);
+    }
+
+    private void Jump()
+    {
+        rb.velocity = new Vector2(rb.velocity.x, jumpspeed);
+        isJumping = true;
+    }
+
+    private void GroundCheck()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheckCollider.position, groundCheckRadius, groundLayer);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,115 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    [SerializeField] private float worldLimtmin;
+    [SerializeField] private float worldLimtmax;
+
+    [SerializeField] private float speed;
+    [SerializeField] private float jumpspeed;
+
+    private Stats stats;
+    private Raycast ray;
+    private CharacterAnimations characterAnimations;
+    private Rigidbody2D rb;
+
+    private float move;
+    public bool isJumping;
+    public bool isGrounded;
+
+    [SerializeField] private bool isFacingRight;
+
+    [SerializeField] private float groundCheckRadius = 0.2f;
+    [SerializeField] Transform groundCheckCollider;
+    [SerializeField] LayerMask groundLayer;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        characterAnimations = GetComponent<CharacterAnimations>();
+        stats = GetComponent<Stats>();
+        ray = GetComponent<Raycast>();
+    }
+
+    private void Update()
+    {
+
+        GroundCheck();
+        move = Input.GetAxis("Horizontal");
+
+        rb.velocity = new Vector2(speed * move, rb.velocity.y);
+
+        if (Input.GetKeyDown(KeyCode.UpArrow) && isGrounded) {
+
+            rb.velocity = new Vector2(rb.velocity.x, jumpspeed);
+        }
+        if (Input.GetKeyDown(KeyCode.Z)) {
+            Attack();
+
+        }
+        if (move >= 0.1f || move <= -0.1f) {
+
+            characterAnimations.states = CharacterAnimations.States.Walk;
+
+
+            if (!isFacingRight && move > 0f) {
+                Flip();
+            }
+            else if (isFacingRight && move < 0f) {
+                Flip();
+            }
+        }
+        else { characterAnimations.states = CharacterAnimations.States.Idle; }
+
+        GroundCheck();
+    }
+
+    private void GroundCheck()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheckCollider.position, groundCheckRadius, groundLayer);
+    }
+
+    private void Flip()
+    {
+        isFacingRight = !isFacingRight;
+        Vector3 localScale = transform.localScale;
+        localScale.x *= -1f;
+        transform.localScale = localScale;
+    }
+
+    private void Attack()
+    {
+        characterAnimations.Punch();
+
+        if (ray.LookRight() == null) return;
+
+        if (isFacingRight) {
+
+            float distance = Vector3.Distance(transform.position, ray.LookRight().transform.position);
+        }
+        else {
+
+            float distance = Vector3.Distance(transform.position, ray.LookLeft().transform.position);
+
+            if (distance < 2f) {
+                ray.LookRight().GetComponent<Stats>().TakeDamage(stats.damage);
+            }
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
