712f4fb671ad3fb0af443a180462123952cd2f43
Locochavo <60815365+Locochavo@users.noreply.github.com>
2023-02-27 13:26:59 -0600

    Add files via upload

---
+++ CharacterAnimations.cs
@@ -0,0 +1,86 @@
+using UnityEngine;
+
+public class CharacterAnimations: MonoBehaviour
+{
+    public States states;
+    private Animator anim;
+
+    public enum States
+    {
+        Walk,
+        Idle
+    }
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    private void DoAnimation(bool Idle, bool Walk)
+    {
+        anim.SetBool("IsIdle", Idle);
+        anim.SetBool("IsWalking", Walk);    
+    }
+
+    private void Walk()
+    {
+        DoAnimation(false, true);
+    }
+ 
+
+    private void Idle()
+    {
+        DoAnimation(true, false);
+    }
+
+    public void Punch()
+    {
+        anim.SetTrigger("IsPunch");
+    }
+
+    private void Update()
+    {
+        StateSwitch();
+    }
+
+    private void StateSwitch()
+    {
+        switch (states)
+        {
+            case States.Idle:
+                {
+                    Idle();
+                    break;
+                }
+            case States.Walk:
+                {
+
+
+                    Walk();
+
+                    break;
+                }           
+              default:
+                {
+                    break;
+                }          
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
